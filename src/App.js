a325443b07cafce7ea0653baf23919180ee085e6
Alexander Diekmann <alexander.diekmann2@googlemail.com>
2018-11-22 11:05:45 +0100

    styling done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {Robots} from './Robots';
+import './App.css';
 
 class App extends Component {
 	constructor() {
@@ -13,18 +14,18 @@ class App extends Component {
 	}
 
 	onSearchChange = (event) => {
-		const filteredRobots = this.state.robots.filter(robots =>{
-			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-		})
-		console.log(filteredRobots);
+		this.setState({searchfield: event.target.value})
 	}
 
 	render() {
+		const filteredRobots = this.state.robots.filter(robots =>{
+		return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
 		return(
 			<div className='tc'>
-				<h1> RoboFriends</h1>
+				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList Robots={this.state.robots} />
+				<CardList Robots={filteredRobots} />
 			</div>
 		);
 	}
