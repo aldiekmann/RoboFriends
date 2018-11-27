c9bf2a3933316daf168f8ef63ad00739a05de385
Alexander Diekmann <alexander.diekmann2@googlemail.com>
2018-11-27 14:52:31 +0100

    Updates

---
+++ precache-manifest.9b73a05547361baa105c7c1cef74f5b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "38b6b5afa1b247434cd6",
+    "url": "/RoboFriends/static/js/main.38b6b5af.chunk.js"
+  },
+  {
+    "revision": "7048bc5c0786f25523ae",
+    "url": "/RoboFriends/static/js/1.7048bc5c.chunk.js"
+  },
+  {
+    "revision": "38b6b5afa1b247434cd6",
+    "url": "/RoboFriends/static/css/main.f6a5e5a0.chunk.css"
+  },
+  {
+    "revision": "7048bc5c0786f25523ae",
+    "url": "/RoboFriends/static/css/1.a79edff1.chunk.css"
+  },
+  {
+    "revision": "dc820c0514202c02c6a5b62a2880cbb2",
+    "url": "/RoboFriends/index.html"
+  }
+];
